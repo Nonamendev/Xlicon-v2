bc0969c5d5f5002a89f1d350b80f7c8817bc980f
johntezali <125339286+johntezali@users.noreply.github.com>
2023-09-15 11:21:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 
 
 //
-global.Owner = ["+254716637803","+254716637803"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["+254716637803","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.Owner = ["+254716637803","+254716637803"];         //If you want singal number so global.Owner = ['+254716637803'] Change into your number.
+global.OwnerNumber = ["+254716637803","8801928502256"];   //If you want singal number so global.Owner = ['254716637803'] Change into your number.
 global.ownertag = ["8801928502256"];
 global.OwnerName = "kingpin ali";
 global.BotName = "XLICON-MD";
